596620337d2b7fbc7ecd72d0576648f7fef87fd0
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2023-12-31 13:10:08 +0530

    section 1,2,3 almost done

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,135 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import MiniCard from "./components/Cards/MiniCard/MiniCard";
 import TrendCard from "./components/Cards/TrendCard/TrendCard";
+import CategoryCard, {
+  CategoryCardMedium,
+  CategoryCardSmall,
+} from "./components/Cards/CategoryCard/CategoryCard";
+
+// assets
+import TeeShirt from "/src/assets/tee-shirts.png";
 
 function App() {
   const [isOffer] = useState(true);
 
+  const smallCardData = [
+    {
+      image: TeeShirt,
+      title: "Jackets",
+    },
+    {
+      image: "",
+      title: "Pastel Stories",
+    },
+    {
+      image: "",
+      title: "Striped Shirts",
+    },
+  ];
+
+  const mediumCardData = [
+    {
+      image: "",
+      title: "Colour Splash",
+    },
+    {
+      image: "",
+      title: "Printed Sweatshirt",
+    },
+    {
+      image:
+        "https://images.bewakoof.com/t640/men-s-black-hope-t-shirt-300994-1655813841-1.jpg",
+      title: "Tee Shirts",
+    },
+  ];
+
   return (
     <>
       <Header isOffer={isOffer} />
-      <section className="section-1">
-        <MiniCard />
-        <TrendCard />
-        <div>
-          <h3 className="design">Design of the week</h3>
-          <img src="/src/assets/best-sellers.png" alt="design of the week" />
-        </div>
-      </section>
 
-      <section>2</section>
+      <div className="parent-container">
+        <div className="container">
+          <section className="section-1">
+            <MiniCard />
+            <TrendCard />
+          </section>
+
+          <section className="section-2">
+            <div className="design--div">
+              <h3 className="design--heading">Design of the week</h3>
+              <div className="design--wrapper">
+                <a href="">
+                  <img
+                    src="https://images.bewakoof.com/uploads/grid/app/DOTW-Split-banner-Desktop-Men-1703576682.jpg"
+                    alt="shop men"
+                    style={{
+                      borderTopLeftRadius: "2rem",
+                      borderBottomLeftRadius: "2rem",
+                    }}
+                  />
+                </a>
+                <a href="">
+                  <img
+                    src="https://images.bewakoof.com/uploads/grid/app/DOTW-Split-banner-Desktop-Women-1703576683.jpg"
+                    alt="shop women"
+                    style={{
+                      borderTopRightRadius: "2rem",
+                      borderBottomRightRadius: "2rem",
+                    }}
+                  />
+                </a>
+              </div>
+            </div>
+          </section>
+
+          <section className="section-3">
+            <h3 className="h3-font-style">Trending Categories</h3>
+            <p className="p-14">
+              Stay ahead of the fashion game with these hot trends to try!
+            </p>
+            <div className="category-wrapper">
+              <div className="category-wrapper--child">
+                <div>
+                  <CategoryCardMedium />
+                </div>
+                <div>
+                  {smallCardData.map((smallCardData) => {
+                    return <CategoryCardSmall smallCardData={smallCardData} />;
+                  })}
+                  {/* <CategoryCardSmall smallCardData={smallCardData} /> */}
+                </div>
+              </div>
+              {/* <div className="category-wrapper--child">
+                <div>
+                  <CategoryCardSmall smallCardData={smallCardData} />
+                </div>
+                <div>
+                  <CategoryCardMedium />
+                </div>
+              </div>
+              <div className="category-wrapper--child">
+                <div>
+                  <CategoryCardMedium />
+                </div>
+                <div>
+                  <CategoryCardSmall smallCardData={smallCardData} />
+                </div>
+              </div> */}
+            </div>
+            {/* <CategoryCard /> */}
+          </section>
+
+          <section className="section-4">
+            <h3 className="h3-font-style">Too hot to be missed</h3>
+            <div className="section-4--div">
+              <div className="section-4--child">1</div>
+              <div className="section-4--child">2</div>
+            </div>
+          </section>
+        </div>
+      </div>
+
+      <section className="footer">footer</section>
     </>
   );
 }
---
+++ src/components/Cards/CategoryCard/CategoryCard.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import "./CategoryCard.css";
+
+type CategoryCard = {
+  isCategoryCardSmall?: boolean;
+  isCategoryCardMedium?: boolean;
+  smallCardData?: any;
+  mediumCardData?: any;
+};
+
+// const smallCardData = [
+//   {
+//     image: "/src/assets/tee-shirts.png",
+//     title: "Jackets",
+//   },
+//   {
+//     image: "",
+//     title: "Pastel Stories",
+//   },
+//   {
+//     image: "",
+//     title: "Striped Shirts",
+//   },
+// ];
+
+// const mediumCardData = [
+//   {
+//     image: "",
+//     title: "Colour Splash",
+//   },
+//   {
+//     image: "",
+//     title: "Printed Sweatshirt",
+//   },
+//   {
+//     image:
+//       "https://images.bewakoof.com/t640/men-s-black-hope-t-shirt-300994-1655813841-1.jpg",
+//     title: "Tee Shirts",
+//   },
+// ];
+
+export const CategoryCardSmall = (props: any) => {
+  console.log(props, "propsssss");
+  console.log(props.title, "propsssss titke");
+  const item = Object.values(props);
+  return (
+    <>
+      {/* {props.smallCardData.map((item: any) => {
+        return (
+          <div
+            className="category-card--small"
+            style={{ backgroundImage: `url("${item.image}")` }}
+          >
+            <button className="btn-white">{item.title}</button>
+          </div>
+        );
+      })} */}
+      
+      {/* {Object.entries(items).map(([key, value]) => {
+        return (
+          <div
+            className="category-card--small"
+            style={{ backgroundImage: `url("${props.image}")` }}
+          >
+            <button className="btn-white">{props.title}</button>
+          </div>
+        );
+      })} */}
+    </>
+  );
+};
+
+export const CategoryCardMedium = (props: any) => {
+  return (
+    <div
+      className="category-card--medium"
+      // style={{ backgroundImage: `url("${item.image}")` }}
+    >
+      {/* <img src="" alt="image" /> */}
+      <button className="btn-white">btn-title</button>
+    </div>
+  );
+};
+
+const CategoryCard = ({
+  isCategoryCardSmall,
+  isCategoryCardMedium,
+  smallCardData,
+  mediumCardData,
+}: CategoryCard) => {
+  return (
+    <div>
+      {/* {isCategoryCardSmall ? (
+        <CategoryCardSmall smallCardData={smallCardData} />
+      ) : null}
+      {isCategoryCardMedium ? <CategoryCardMedium /> : null} */}
+      <CategoryCardSmall smallCardData={smallCardData} />
+      <CategoryCardMedium mediumCardData={mediumCardData} />
+    </div>
+  );
+};
+
+export default CategoryCard;
