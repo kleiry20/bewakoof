a52b1e11a0c5949ff09793abcc1876eb097e5294
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-01-01 17:20:39 +0530

    header responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,47 +1,59 @@
 import React, { useState } from "react";
 import "./Header.css";
 import SearchBar from "../SearchBar/SearchBar";
+import {
+  Button,
+  Container,
+  Form,
+  Nav,
+  NavDropdown,
+  Navbar,
+} from "react-bootstrap";
+import SideBar from "../SideBar/SideBar";
 
 const Header = (props: any) => {
   return (
-    <header className="header">
-      {props.isOffer == true ? (
-        <div className="offer">
-          <p className="offer-text">Flat 10% off on your first order</p>
-        </div>
-      ) : null}
-
-      <div className="navbar">
-        <div className="navbar--left">
-          <img
-            src="https://images.bewakoof.com/web/ic-desktop-bwkf-trademark-logo.svg"
-            alt="bewakoof"
-            style={{ width: "10rem" }}
-          />
-          <a href="">Men</a>
-          <a href="">Women</a>
-          <a href="">Accessories</a>
-        </div>
-
-        <div className="navbar--right">
-          <div>
-            <SearchBar />{" "}
+    <>
+      <SideBar />
+      <header className="header desktop-view">
+        {props.isOffer == true ? (
+          <div className="offer">
+            <p className="offer-text">Flat 10% off on your first order</p>
           </div>
-          <a href="">
-            {" "}
-            <i className="fas fa-user"></i> Account
-          </a>
-          <a href="">
-            {" "}
-            <i className="fas fa-heart"></i> Wishlist
-          </a>
-          <a href="">
-            {" "}
-            <i className="fas fa-bag-shopping"></i> Cart
-          </a>
+        ) : null}
+
+        <div className="navbar">
+          <div className="navbar--left">
+            <img
+              src="https://images.bewakoof.com/web/ic-desktop-bwkf-trademark-logo.svg"
+              alt="bewakoof"
+              style={{ width: "10rem" }}
+            />
+            <a href="">Men</a>
+            <a href="">Women</a>
+            <a href="">Accessories</a>
+          </div>
+
+          <div className="navbar--right">
+            <div>
+              <SearchBar />{" "}
+            </div>
+            <a href="">
+              {" "}
+              <i className="fas fa-user"></i> Account
+            </a>
+            <a href="">
+              {" "}
+              <i className="fas fa-heart"></i> Wishlist
+            </a>
+            <a href="">
+              {" "}
+              <i className="fas fa-bag-shopping"></i> Cart
+            </a>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+function SideBar() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Navbar
+        className="mobile-view"
+        style={{
+          backgroundColor: "#ffc303",
+          display: "flex",
+          gap: "2rem",
+          padding: "1rem",
+          justifyContent: "space-between",
+        }}
+      >
+        <div style={{ display: "flex", gap: "6px", alignItems: "center" }}>
+          <i onClick={handleShow} className="fas fa-bars mt-1"></i>
+          <a href="">
+            <img
+              src="https://images.bewakoof.com/web/ic-desktop-bwkf-trademark-logo.svg"
+              alt="bewakoof"
+              style={{ width: "7rem" }}
+            />
+          </a>
+        </div>
+        <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+          <a href="">
+            {" "}
+            <i className="fas fa-user"></i>
+          </a>
+          <a href="">
+            {" "}
+            <i className="fas fa-heart"></i>
+          </a>
+          <a href="">
+            {" "}
+            <i className="fas fa-bag-shopping"></i>
+          </a>
+        </div>
+        {/* <Container>
+          <i onClick={handleShow} className="fas fa-bars"></i>
+          <Navbar.Brand href="#home">
+            <img
+              src="https://images.bewakoof.com/web/ic-desktop-bwkf-trademark-logo.svg"
+              alt="bewakoof"
+              style={{ width: "7rem" }}
+            />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Link</Nav.Link>
+            <Nav.Link href="#pricing">Actions</Nav.Link>
+          </Nav>
+        </Container> */}
+      </Navbar>
+
+      <Offcanvas className="only-mobile" show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Link</Nav.Link>
+            <Nav.Link href="#pricing">Actions</Nav.Link>
+          </Nav>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+}
+
+export default SideBar;
